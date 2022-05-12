bc4421eaec26dea4b21a4432ad3aae75b3f3794b
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-05-11 22:01:26 -0400

    further progress on the APIn

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //alert('It is linked')
+
+//VARIABLES
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const URLtwo = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin';
+const URLthree = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka'
+
+
+const $getCocktailbtn = $('.getCocktailbtn')
+
+//FUNCTIONS
+
+const getCocktail = () =>{
+    $.ajax(URL).then(function (data){
+        
+        console.log(data)
+        console.log(data.drinks[0].strIngredient1)
+        $cocktailName.text(data.drinks[0].strDrink)
+        $ingredient.text(data.drinks[0].strIngredient1)
+        $('main').append(`<img src="${data.drinks[0].strDrinkThumb}"/>`)
+    })
+    
+};
+
+//ELEMENT REFERENCES
+const $cocktailName = $('#cocktailName')
+const $ingredient = $('#ingredient');
+const $form = $('form');
+const $input = $(`input[type="text"]`);
+
+//EVENT LISTENERS
+$getCocktailbtn.on('click',getCocktail);
